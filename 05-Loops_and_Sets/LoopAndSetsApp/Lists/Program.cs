b43c408ffb6aee78380af2e035970504ad21e542
Alberto Noguchi <alberto2611@gmail.com>
2022-02-27 19:46:35 -0300

    add: section loops & sets: lists

---
+++ 05-Loops_and_Sets/LoopAndSetsApp/Lists/Program.cs
@@ -0,0 +1,24 @@
+﻿// Is like array but with new additionals
+// If you have a set of datas use Lists
+// List<T>: generic
+
+List<string> firstNames = new List<string>();
+firstNames.Add("Beto");
+firstNames.Add("Julia");
+firstNames.Add("Alberto");
+firstNames.Add("Paula");
+
+Console.WriteLine(firstNames[0]);
+Console.WriteLine(firstNames[firstNames.Count - 1]);
+
+List<int> ages = new List<int>();
+ages.Add(1);
+ages.Add(2);
+ages.Add(3);
+
+string data = "Alberto,Julia,Beto";
+
+// Array to List
+List<string> names = data.Split(',').ToList();
+names.Add("Paula");
+
---
+++ 05-Loops_and_Sets/LoopAndSetsApp/PracticeLists/Program.cs
@@ -0,0 +1,6 @@
+﻿// Add students to a class roster List until there are no more students. Then print out the count of the students to the console.
+
+string data = "Alberto,Julia,Beto,Paula,Ana,Amanda";
+
+List<string> classRoster = data.Split(',').ToList();
+Console.WriteLine(classRoster.Count);
