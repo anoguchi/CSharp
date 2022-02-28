c7884b868b64d5cd3acfce8148c7158b1e6b042c
Alberto Noguchi <alberto2611@gmail.com>
2022-02-28 00:39:11 -0300

    add: section loops & sets: for each loops

---
+++ 05-Loops_and_Sets/LoopAndSetsApp/ForEachLoops/Program.cs
@@ -0,0 +1,10 @@
+﻿// ForEach is better! Use for when you need a counter, when you know how much elements you have in a list.
+
+string data = "Alberto,Julia,Ana,Paula";
+List<string> lists = data.Split(',').ToList();
+
+
+foreach (string list in lists)
+{
+    Console.WriteLine(list);
+}
---
+++ 05-Loops_and_Sets/LoopAndSetsApp/PracticeForEachLoops/Program.cs
@@ -0,0 +1,20 @@
+﻿// Ask the user for their first name. Continue asking for first names until there are no more. Then loop through each name using foreach and tell each person hello on the console.
+
+string name;
+List<string> lists = new();
+
+do
+{
+    Console.Write("What is your first name? (Type exit to quit application) ");
+    name = Console.ReadLine();
+    
+    lists.Add(name);
+
+} while (name.ToLower() != "exit");
+
+lists.RemoveAt(lists.Count - 1);
+
+foreach (string list in lists)
+{
+    Console.WriteLine($"Hello {list}");
+}
